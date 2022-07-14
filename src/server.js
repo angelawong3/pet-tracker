616aaf398dc1dbfc1444cfa935f2ed94c69e99da
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-14 21:07:04 +0100

    added const sess

--- src/server.js
+++ src/server.js
@@ -9,6 +9,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
