d84ac536587d9d888daf4e5eeb83a594d5d15b97
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-14 19:42:16 +0100

    added /login and /signup routes

--- src/controllers/homeRoutes.js
+++ src/controllers/homeRoutes.js
@@ -5,29 +5,35 @@ const { Pet, User } = require('../models');
 
 // get all posts
 router.get('/', async (req, res) => {
-  res.render("homepage");
+  res.render('homepage');
   // swap for res.render when handlebars is configured
 });
 
 // dashboard route
 router.get('/', async (req, res) => {
-
-  res.render("dashboard", {
-    logged_in: req.session.logged_in 
-  }); 
+  res.render('dashboard', {
+    logged_in: req.session.logged_in,
+  });
 });
-
 
 // login route
 router.get('/login', (req, res) => {
-  res.render("login");
-  // swap for res.render when handlebars is configured
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
 });
 
 // signup route
 router.get('/signup', (req, res) => {
-  res.render("signup");
-  // swap for res.render when handlebars is configured
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
