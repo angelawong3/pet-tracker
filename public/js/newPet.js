00f81bb4fe0872d6a10cc2118fe9f5ad8599230d
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-15 14:06:07 +0100

    added new,edit,and del function

--- public/js/newPet.js
+++ public/js/newPet.js
@@ -1,1 +1,29 @@
 // event handler for button click to save pet
+// to be updated
+
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const postTitle = document.querySelector('#post-title').value.trim();
+  const postContent = document.querySelector('#post-content').value.trim();
+
+  if (postTitle && postContent) {
+    const response = await fetch(`/api/post`, {
+      method: 'POST',
+      body: JSON.stringify({ postTitle, postContent }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
