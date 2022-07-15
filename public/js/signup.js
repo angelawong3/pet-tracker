e646eb45c5ae81cf72fc24037a262233a7aebf07
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-07-15 14:02:06 +0100

    added signup eventHandler

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,1 +1,24 @@
-// event handler for button click
+// to be updated
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#name-signup').value;
+  const password = document.querySelector('#password-signup').value;
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({ username, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
