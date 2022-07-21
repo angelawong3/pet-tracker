1e3ad6a746b9f8b5ad53b283446510b8892adb8f
aali9 <74592306+aali9@users.noreply.github.com>
2022-07-21 21:46:06 +0100

    changes

--- __test__/login.test.js
+++ __test__/login.test.js
@@ -1,43 +1,37 @@
-const Login = require ('./login.js');
+const Login = require('./login.js');
 
-describe('POST /users'), () => {
-  describe('given a username and password'), () => {
+describe('POST /users', () => {
+  it('should respond with a 200 status code', async () => {
+    const response = await request(Login).post('./login.js').send({
+      username: 'username',
+      password: 'password',
+    });
+    expect(response.statusCode).toBe(200);
+  });
 
- 
-    it('should respond with a 200 status code', async () => {
-      const response = await request(app).post("./login.js").send({
-        username: "username",
-        password: "password"
-      })
-      expect(response.statusCode).toBe(200)
-    })
- }}
-    it('should specify json in the content type header', async () => {
-      const response = await request(app).post("./login.js").send({
-        username: "username",
-        password: "password"
-      })
-      expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
-    })
-    it('response has userId', async () => {
-      const response = await request(app).post("./login.js").send({
-        username: "username",
-        password: "password"
-      })
-      expect(response.body.userId).toBeDefined()
-    })
-  
+  it('should specify json in the content type header', async () => {
+    const response = await request(Login).post('./login.js').send({
+      username: 'username',
+      password: 'password',
+    });
+    expect(response.headers['content-type']).toEqual(
+      expect.stringContaining('json')
+    );
+  });
 
-  describe('when the username and password is missing', () => {
-    it("should respond with a status code of 400", async () => {
-      const bodyData = [
-        {username: "username"},
-        {password: "password"},
-        {}
-      ]
-      for (const body of bodyData) {
-        const response = await request(app).post("./login.js").send(body)
-        expect(response.statusCode).toBe(400)
-    }})}
-  })};
-  
+  it('response has userId', async () => {
+    const response = await request(Login).post('./login.js').send({
+      username: 'username',
+      password: 'password',
+    });
+    expect(response.body.userId).toBeDefined();
+  });
+
+  it('should respond with a status code of 400 when un/pw is missing', async () => {
+    const bodyData = [{ username: 'username' }, { password: 'password' }, {}];
+    for (const body of bodyData) {
+      const response = await request(app).post('./login.js').send(body);
+      expect(response.statusCode).toBe(400);
+    }
+  });
+});
