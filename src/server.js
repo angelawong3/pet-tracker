813d335cfb7afdb77d452e40740e0c02ae3e2a1b
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-07-22 11:38:49 +0100

    temp use js cdn to render js components on site

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -27,7 +26,6 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, '../public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
